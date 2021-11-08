f7fbc3f42bd8a219dca3195ceb1dc59b3ae2ac3e
Luska159 <lucasdenofrio@hotmail.com>
2021-11-08 19:11:09 -0300

    ....

--- AssociacaoAcademia/test/pacote/clienteDao/ClienteDaoImpTest.java
+++ AssociacaoAcademia/test/pacote/clienteDao/ClienteDaoImpTest.java
@@ -11,8 +11,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
